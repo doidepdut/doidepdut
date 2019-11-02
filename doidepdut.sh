925e83b4aa10bb285463e1ae6b1f64f8b2b1e5ed
doidepdut <32607304+doidepdut@users.noreply.github.com>
2019-11-02 11:31:10 +0700

    Update doidepdut.sh

--- doidepdut.sh
+++ doidepdut.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install git build-essential screen libuv1-dev libmicrohttpd-dev make automake cmake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
-sudo sysctl -w vm.nr_hugepages=$((grep -c ^processor /proc/cpuinfo * 3))
-git clone https://github.com/webchain-network/webchain-miner.git && cd webchain-miner
+sudo apt-get -y install git build-essential screen libuv1-dev libmicrohttpd-dev make automake cmake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev cpulimit
+cd /usr/local/src/
+git clone https://github.com/uPlexa/xmrig-upx.git && cd xmrig-upx
 mkdir build
 cd build
-cmake ..
+cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF -DMICROHTTPD_ENABLE=OFF
 make 
-chmod +x webchain-miner
-screen -S webchain-miner
-screen -d -m ./webchain-miner -o pool.webchain.network:3333 -u 0xd73051a93ebc69b885f645fb80e5336aa03ade49 -p x -t 1 --worker-id 2 --donate-level=1
+chmod +x xmrig
+sysctl -w vm.nr_hugepages=128
+echo "vm.nr_hugepages=128" >> /etc/sysctl.conf
+echo "[Unit]" >> /lib/systemd/system/trtl.service
+echo "Description=trtr" >> /lib/systemd/system/trtl.service
+echo "After=network.target" >> /lib/systemd/system/trtl.service
+echo "[Service]" >> /lib/systemd/system/trtl.service
+echo "ExecStart=/usr/local/src/xmrig-upx/build/xmrig --donate-level 1 -o uplexa.herominers.com:10471 -u UPX1YvYKwEyVHcB82dVebfUcwLrABC39GVvC7GChmq4L1KruLpL8ntJGr1hcXXCs6m68RBk2TmhDPgW9fP6S9e2k2h6iUQVnLS -p keypair -a cryptonight-upx/2 -k">> /lib/systemd/system/trtl.service
+echo "User=root" >> /lib/systemd/system/trtl.service
+echo "[Install]" >> /lib/systemd/system/trtl.service
+echo "WantedBy=multi-user.target" >> /lib/systemd/system/trtl.service
+sudo systemctl daemon-reload
+sudo systemctl enable trtl.service
+sudo systemctl start trtl.service
